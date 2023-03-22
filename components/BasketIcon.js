30b462597e4d3a2a5d9b2d01a797f7217c8ece9c
Cristian Ceballos <cmceballos88@gmail.com>
2023-03-22 15:32:07 +0100

    Add view basket Component and add selectBAsketTotal method to the redux

---
+++ components/BasketIcon.js
@@ -0,0 +1,33 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectBaskedTotal, selectBasketItems } from "../features/basketSlice";
+import { useNavigation } from "@react-navigation/native";
+import { formatCurrency } from "react-native-format-currency";
+
+const BasketIcon = () => {
+  const items = useSelector(selectBasketItems);
+  const basketTotal = useSelector(selectBaskedTotal);
+  const navigation = useNavigation();
+  const [valueFormattedWithSymbol] = formatCurrency({
+    amount: basketTotal,
+    code: "USD"
+  });
+  return (
+    <View className="absolute bottom-10 w-full z-50">
+      <TouchableOpacity className="bg-[#00CCBB] mx-5 p-4 rounded-lg flex-row items-center space-x-1">
+        <Text className="text-white font-extrabold text-lg bg-[#01A296] py-1 px-2">
+          {items.length}
+        </Text>
+        <Text className="text-white font-extrabold text-lg text-center flex-1">
+          View Basket
+        </Text>
+        <Text className="text-lg text-white font-extrabold">
+          {valueFormattedWithSymbol}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default BasketIcon;
--- features/basketSlice.js
+++ features/basketSlice.js
@@ -28,4 +28,7 @@ export const selectBasketItems = (state) => state.basket.items;
 export const selectBasketItemsById = (state, id) =>
   state.basket.items.filter((item) => item.id === id);
 
+export const selectBaskedTotal = (state) =>
+  state.basket.items.reduce((total, item) => (total += item.price), 0);
+
 export default basketSlice.reducer;
