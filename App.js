03bd0cf9b55e9a269bd74b629cc885f2ffb23d9e
Cristian Ceballos <cmceballos88@gmail.com>
2023-03-22 15:46:30 +0100

    Create BasketScreen and add the navigation for it

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import HomeScreen from "./screens/HomeScreen";
 import RestaurantScreen from "./screens/RestaurantScreen";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import BasketScreen from "./screens/BasketScreen";
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
@@ -13,6 +14,11 @@ export default function App() {
         <Stack.Navigator>
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+          <Stack.Screen
+            name="Basket"
+            component={BasketScreen}
+            options={{ presentation: "modal", headerShown: false }}
+          />
         </Stack.Navigator>
       </Provider>
     </NavigationContainer>
